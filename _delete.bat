d4b3be0cdcf262ce76c861534400e12ab9f8388d
Jan Rother <janrother02@gmail.com>
2022-10-29 03:16:34 +0200

    add initial version of template thesis

---
+++ _delete.bat
@@ -0,0 +1,20 @@
+del *.aux
+del *.bbl
+del *.blg
+del *.bak
+del *.bcf
+del *.fdb_latexmk
+del *.fls
+del *.gz
+del *.lof
+del *.log
+del *.lot
+del *.lol
+del *.out
+del *.run.xml
+del *.synctex.gz
+del *.tdo
+del *.toc
+del *blx.bib
+
+REM del *.pdf
